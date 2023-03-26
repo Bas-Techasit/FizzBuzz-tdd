ae2c7c5600d8f2b450715114be3b1ca454cb7046
Bas-Techasit <techasit.bas@gmail.com>
2023-03-26 20:29:36 +0700

    completed

--- test/test.js
+++ test/test.js
@@ -1,5 +1,26 @@
+const { fizzBuzz } = require('../index')
 
+describe('Create the fizzBuzz method', function () {
 
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4)
+    test('Should be return string', () => {
+        const result = fizzBuzz(1)
+        expect(typeof result).toBe('string');
+    })
+
+    test('When pass 3 as parameter it should be return "Fizz". ', () => {
+        const result = fizzBuzz(3)
+        expect(result).toBe('Fizz')
+    })
+
+    test('When pass 5 as parameter it should be return "Buzz".', () => {
+        const result = fizzBuzz(5)
+        expect(result).toBe('Buzz')
+    })
+
+    test('If the parameter equal to 3*5 it should be return "FizzBuzz"', () => {
+        const result = fizzBuzz(3*5)
+        expect(result).toBe('FizzBuzz')
+    })
+
 })
+
